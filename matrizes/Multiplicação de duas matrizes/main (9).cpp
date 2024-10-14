fb48d64a112f7801be0c167da2690d8ecefdc8f0
TiagoPaschoalino <tipaschoalino@gmail.com>
2024-10-14 15:28:43 -0300

    Multiplicação de duas matrizes

---
+++ matrizes/Multiplicação de duas matrizes/main (9).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int matriz1[3][3], matriz2[3][3], matrizSoma[3][3];
+
+    // Leitura da primeira matriz 3x3
+    cout << "Digite os elementos da primeira matriz 3x3:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matriz1[i][j];
+        }
+    }
+
+    // Leitura da segunda matriz 3x3
+    cout << "Digite os elementos da segunda matriz 3x3:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matriz2[i][j];
+        }
+    }
+
+    // Multiplicação das duas matrizes
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrizSoma[i][j] = matriz1[i][j] * matriz2[i][j];
+        }
+    }
+
+    // Exibição da matriz resultante da Multiplicação
+    cout << "Matriz resultante da Multiplicação:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrizSoma[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
